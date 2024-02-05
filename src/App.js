c4a9cf877b2a4862341d9b964b78df98a7229698
Hanna Nesterova <nesterova.hanna@gmail.com>
2024-02-06 00:53:23 +0100

    creaste navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
-import './App.css';
+import styles from './App.module.css';
+import Header from './Components/Header/Header';
 
 function App() {
   return (
-    <div className="App">
-
+    <div className={styles.App}>
+      <Header />
     </div>
   );
 }
