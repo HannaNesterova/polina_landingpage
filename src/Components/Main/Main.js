69fc37bcf86371058a0d8208e55dd9bb240ab4ee
Hanna Nesterova <nesterova.hanna@gmail.com>
2024-02-17 18:59:57 +0100

    Main button

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,9 +1,9 @@
 import { useEffect } from 'react';
+import { Link } from 'react-scroll';
 import hero from '../../img/hero/Krylova.png';
 import styles from './Main.module.css';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
-
 
 function Main(){
 
@@ -11,15 +11,13 @@ function Main(){
         AOS.init();
         AOS.refresh();
       }, []);
-
-
     return(
         <div className={styles.container_cont}>
         <div className={styles.main_container}>
         <div className={styles.first_cont} data-aos="fade-up" data-aos-duration="3000">
             <h1>Polins_pl</h1>
             <p>Добро пожаловать на авторский курс <br /> „Посредника по аренде недвижимости в Польше»</p>
-            <button className={styles.btn}>Записаться на курс</button>
+            <Link to="packages-section"><button className={styles.btn}>Записаться на курс</button></Link>
             <div className={styles.start_comp}>
                 <div className={styles.date}>
                     <h2>Старт</h2>
