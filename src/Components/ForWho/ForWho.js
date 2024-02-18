7a2ddccea6c1f7bbc756290ebd7eabf1e52576b0
Hanna Nesterova <nesterova.hanna@gmail.com>
2024-02-18 18:56:12 +0100

    Add aos for ForWho components

--- src/Components/ForWho/ForWho.js
+++ src/Components/ForWho/ForWho.js
@@ -1,6 +1,14 @@
+import { useEffect } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 import styles from '../ForWho/ForWho.module.css';
 
 function ForWho(){
+    useEffect(() => {
+        AOS.init();
+        AOS.refresh();
+      }, []);
+
     return(
         <div>
             <div className={styles.container}>
